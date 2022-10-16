3445e5f2f127b99aac9205725cc259cbf17c18e1
SiamAnzir <siamanzir18@gmail.com>
2022-10-17 00:49:24 +0600

    [Added] all-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import NavBar from "./components/Navbar";
 import Home from "./components/Home";
+import Products from "./components/Products";
+import Contact from "./components/Contact";
 import './App.css';
 
 const App = () => {
@@ -9,6 +11,8 @@ const App = () => {
         <section>
             <NavBar></NavBar>
             <Home></Home>
+            <Products></Products>
+            <Contact></Contact>
         </section>
     </>
   );
