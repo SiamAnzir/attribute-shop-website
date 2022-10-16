5f0ea015b451ac297fe5020aff412dc683c98671
SiamAnzir <siamanzir18@gmail.com>
2022-10-17 00:43:23 +0600

    [Created] AllProducts Component

---
+++ src/store/AllProducts.js
@@ -0,0 +1,147 @@
+import award from "../assets/icon/awards.png";
+import cap from "../assets/icon/cap.png";
+import clock from "../assets/icon/clock.png";
+import giftBox from "../assets/icon/corporate gift box.png";
+import idCard from "../assets/icon/id card.png";
+import keyRing from "../assets/icon/key ring.png";
+import logo from "../assets/icon/Logo.png";
+import mug from "../assets/icon/mug.png";
+import polo from "../assets/icon/polo.png";
+import printPress from "../assets/icon/printing & press works.png";
+import tShirt from "../assets/icon/t-shirt.png";
+import umbrella from "../assets/icon/umbrella.png";
+
+export const allProducts = [
+    {
+        id:1,
+        categoryName:'Awards',
+        subCategories: [
+            {
+                serviceId:'',
+                serviceName:'',
+            }
+        ],
+        logo:award
+    },
+    {
+        id:2,
+        categoryName:'Cap',
+        subCategories: [
+            {
+                serviceId:'',
+                serviceName:'',
+            }
+        ],
+        logo:cap
+    },
+    {
+        id:3,
+        categoryName:'Clock/Watch',
+        subCategories: [
+            {
+                serviceId:'',
+                serviceName:'',
+            }
+        ],
+        logo:clock
+    },
+    {
+        id:4,
+        categoryName:'Corporate Gift Box',
+        subCategories: [
+            {
+                serviceId:'',
+                serviceName:'',
+            }
+        ],
+        logo:giftBox
+    },
+    {
+        id:5,
+        categoryName:'ID Card',
+        subCategories: [
+            {
+                serviceId:'',
+                serviceName:'',
+            }
+        ],
+        logo:idCard
+    },
+    {
+        id:6,
+        categoryName:'Key Ring',
+        subCategories: [
+            {
+                serviceId:'',
+                serviceName:'',
+            }
+        ],
+        logo:keyRing
+    },
+    {
+        id:7,
+        categoryName:'Logo',
+        subCategories: [
+            {
+                serviceId:'',
+                serviceName:'',
+            }
+        ],
+        logo:logo
+    },
+    {
+        id:8,
+        categoryName:'Mug',
+        subCategories: [
+            {
+                serviceId:'',
+                serviceName:'',
+            }
+        ],
+        logo:mug
+    },
+    {
+        id:9,
+        categoryName:'Polo T-Shirt',
+        subCategories: [
+            {
+                serviceId:'',
+                serviceName:'',
+            }
+        ],
+        logo:polo
+    },
+    {
+        id:10,
+        categoryName:'Printing & Press Works',
+        subCategories: [
+            {
+                serviceId:'',
+                serviceName:'',
+            }
+        ],
+        logo:printPress
+    },
+    {
+        id:11,
+        categoryName:'T-Shirt',
+        subCategories: [
+            {
+                serviceId:'',
+                serviceName:'',
+            }
+        ],
+        logo:tShirt
+    },
+    {
+        id:12,
+        categoryName:'Umbrella',
+        subCategories: [
+            {
+                serviceId:'',
+                serviceName:'',
+            }
+        ],
+        logo:umbrella
+    },
+]
