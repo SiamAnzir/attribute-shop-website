ef3270c513653c62ac0a13d849d562280dd5ec45
SiamAnzir <siamanzir18@gmail.com>
2022-10-17 00:45:21 +0600

    [Modified] navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,27 @@
 import React from "react";
 import {Container, Nav, Navbar, NavItem,Form ,Button} from "react-bootstrap";
 import logo from "../assets/attributelogo.png";
+import mainLogo from "../assets/PNG.png";
 
 const NavBar = () => {
     return(
-        <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+        <Navbar bg="white" variant="dark" expand="lg" fixed="top">
             <Container>
                 <Nav className="navbar-brand">
-                    <Nav.Link href="#about" className="text-white text-decoration-none">
-                        <img src={logo} alt="..." height={40}/>
+                    <Nav.Link href="/" className="text-white text-decoration-none">
+                        <img src={mainLogo} alt="..." height={30}/>
                     </Nav.Link>
                 </Nav>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-                    <div>
-                        <Form className="d-flex">
-                            <Form.Control
-                                type="search"
-                                placeholder="Search"
-                                className="me-2"
-                                aria-label="Search"
-                            />
-                            <Button variant="outline-danger">Search</Button>
-                        </Form>
-                    </div>
-                </Navbar.Collapse>
-                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                     <Nav>
-                        <NavItem> <Nav.Link href="#testimonials" className="text-white text-decoration-none"> Services/Products  </Nav.Link> </NavItem>
-                        <NavItem> <Nav.Link href="#contact" className="text-white text-decoration-none"> Contact Us  </Nav.Link> </NavItem>
+                        <div style={{border:"2px solid red",borderRadius:'10px'}}>
+                            <NavItem> <Nav.Link href="#products" className="text-black text-decoration-none"> Services/Products  </Nav.Link> </NavItem>
+                        </div>
+                        <div style={{paddingRight:'20px'}}></div>
+                        <div style={{border:"2px solid red",borderRadius:'10px'}}>
+                            <NavItem> <Nav.Link href="#contact" className="text-black text-decoration-none"> Contact Us  </Nav.Link> </NavItem>
+                        </div>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
