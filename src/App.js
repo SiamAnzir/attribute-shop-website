25556ecfc0a25c572d8cde708ab6d073ec38765b
SiamAnzir <siamanzir18@gmail.com>
2023-03-30 12:47:11 +0600

    modified

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import EachProduct from "./components/EachProduct";
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
-      <Router>
-          <Switch>
-              <Route exact path="/">
-                  <HomePage></HomePage>
-              </Route>
-              <Route path="/product/:name">
-                  <EachProduct></EachProduct>
-              </Route>
-          </Switch>
-      </Router>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage></HomePage>
+        </Route>
+        <Route path="/product/:name">
+          <EachProduct></EachProduct>
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
